8377bcce6e7ae43bea10cd6d6457dda573d6be1a
Gen Phillips <118129965+GxnBytes@users.noreply.github.com>
2025-09-21 12:50:21 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,11 @@
+# vpc.tf
 # Created a VPC
 # Defines the Virtual Private Cloud (VPC)
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-
   tags = {
     Name = "Project VPC"
   }
 }
-
