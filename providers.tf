81b4477c0f2934bc1a53dc487ab8b301be7ae96e
Gen Phillips <118129965+GxnBytes@users.noreply.github.com>
2025-09-21 12:38:03 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,19 @@
+# providers.tf
+# Defines the required Terraform and AWS provider versions
+# and sets the deployment region to eu-north-1
+
+terraform {
+  required_version = ">= 1.13.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
 
