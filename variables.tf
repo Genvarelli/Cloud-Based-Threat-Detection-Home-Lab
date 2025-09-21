9b6c52c42c0fc67bc6f3de89a0a2d1e26fb3f698
Gen Phillips <118129965+GxnBytes@users.noreply.github.com>
2025-09-21 13:19:22 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
 # Input variables
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "allowed_ssh_ip" {
+  description = "IP address allowed for SSH access"
+  type        = string
+  default     = "your.IP/32"  # Replace with your actual IP
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 2
+}
